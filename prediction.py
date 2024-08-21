32989c8d9c4bb48957674c96bfb6ecf873314cd7
aksingh <abhisheksingh@myn.global>
2024-08-21 12:36:16 +0530

    Added code to handle ECI chat inbound requests

--- prediction.py
+++ prediction.py
@@ -116,3 +116,71 @@ def predict(args_dict):
     # chain.run(input_documents=docs, question=query)
     return {'prediction': chain.run(input_documents=docs, question=query)}
 
+
+def eci_predict(args_dict):
+#     arg = args_dict.get('arg1')
+#     predictor(arg)
+    print("LOGGING IN")
+    login(token="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")
+    query = args_dict.get('text').replace('eciai', '')
+    print("Flow : inside predict with query",query)
+    
+    # model = AutoModelForCausalLM.from_pretrained("mistralmnemo")
+    # tokenizer = AutoTokenizer.from_pretrained("mistraltnemo")
+
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-Nemo-Instruct-2407")
+    model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-Nemo-Instruct-2407")
+    # if(os.path.isdir("/mistral-7b-model")):
+    #     print("model present")
+    #     model = AutoModelForCausalLM.from_pretrained("/mistral-7b-model")
+    # if(os.path.isdir("/mistral-7b-tokenizer")):
+    #     tokenizer = AutoTokenizer.from_pretrained("/mistral-7b-tokenizer")
+    # else:
+    #     print("model NOT present")
+    #     tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3")
+    #     model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.3")
+    
+    model.save_pretrained("models/mistralmnemo")
+    tokenizer.save_pretrained("models/mistraltnemo")
+
+    text_generation_pipeline = pipeline(
+    model=model,
+    tokenizer=tokenizer,
+    task="text-generation",
+    temperature=0.2,
+    do_sample=True,
+    repetition_penalty=1.1,
+    return_full_text=True,
+    max_new_tokens=400,
+    device=0
+    )
+
+    llm = HuggingFacePipeline(pipeline=text_generation_pipeline)
+    embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-m3")
+    pages=[]
+    for file in os.listdir("files/"):
+        if file.endswith('.pdf'):
+            pdf_path = os.path.join("files/", file)
+            loader = PyPDFLoader(pdf_path)
+            pages.extend(loader.load_and_split())
+            
+
+    # np_pages = np.array(pages)
+    # np.save('pages',np_pages)
+    # pages = np.load("pages.npy",allow_pickle=True)
+    
+    text_splitter = CharacterTextSplitter(
+        separator = "\n",
+        chunk_size = 1400,
+        chunk_overlap  = 100,
+        length_function = len,
+    )
+
+    combined_content = ''.join([p.page_content for p in pages])
+    texts = text_splitter.split_text(combined_content)
+    document_search = FAISS.from_texts(texts, HuggingFaceEmbeddings(model_name="BAAI/bge-m3"))
+    # document_search = FAISS.from_texts(texts, embed_model)
+    docs = document_search.similarity_search(query)
+    chain = load_qa_chain(llm, chain_type="stuff")
+    # chain.run(input_documents=docs, question=query)
+    return {'prediction': chain.run(input_documents=docs, question=query)}
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask, jsonify, request
 from prediction import predict
+from prediction import eci_predict
 
 application = Flask(__name__)
 
@@ -17,3 +18,24 @@ def create_prediction():
     body = json.loads(data)
     print(body)
     return jsonify(predict(body)["prediction"].rpartition("Helpful Answer:")[-1])
+
+@application.route('/ecichat', methods=['POST'])
+def create_prediction():
+    data = request.data or '{}'
+    body = json.loads(data)
+    print(body)
+    rag_response = eci_predict(body)["prediction"].rpartition("Helpful Answer:")[-1]
+    response_data = {
+        "response_type": "comment",
+        "username": "ECI AI",
+        "icon_url": "https://mattermost.com/wp-content/uploads/2022/02/icon.png",
+        "text": rag_response
+    }
+    response_body = json.dumps(response_data)
+    return response_body
+    # response_headers = [
+    #     ('Content-Type', 'application/json'),
+    #     ('Content-Length', str(len(response_body))),
+    # ]
+    # start_response('200 OK', response_headers)
+    # return jsonify(predict(body)["prediction"].rpartition("Helpful Answer:")[-1])
